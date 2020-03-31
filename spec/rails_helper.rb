3e1ece6ce51a6643bd2eb18a1708770784378b76
James Kessler <afinetooth@gmail.com>
2020-03-30 21:50:20 -0700

    Configure DatabaseCleaner and Shoulda.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,23 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Configure Database Cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  # Configure Shoulda
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end

3bc1a2f6de67551f27ddabb0bf5d8cf818c0ce46
James Kessler <afinetooth@gmail.com>
2020-04-08 13:24:59 -0700

    Make tests pass.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_08_192709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blog_posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+  end
 
 end
